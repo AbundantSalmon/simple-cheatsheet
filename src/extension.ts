07c42743783ba4b3c19de32e5759c2baf21bdc6b
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2022-09-03 21:46:10 +0800

    add basic show and close webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vscode-cheatsheet" is now active!'
-  );
+  let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "vscode-cheatsheet.helloWorld",
+  let showCheatSheetCommand = vscode.commands.registerCommand(
+    "vscode-cheatsheet.showCheatSheet",
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from vscode-cheatsheet!"
+      const columnToShowIn = vscode.window.activeTextEditor
+        ? vscode.window.activeTextEditor.viewColumn
+        : undefined;
+      if (currentPanel) {
+        currentPanel.reveal(columnToShowIn);
+      } else {
+        currentPanel = vscode.window.createWebviewPanel(
+          "vscodeCheatSheet",
+          "VSCode Cheat Sheet",
+          vscode.ViewColumn.One,
+          {}
+        );
+
+        currentPanel.webview.html = getWebviewContent();
+      }
+
+      currentPanel.onDidDispose(
+        () => {
+          currentPanel = undefined;
+        },
+        null,
+        context.subscriptions
       );
     }
   );
 
-  context.subscriptions.push(disposable);
+  let closeCheatSheetCommand = vscode.commands.registerCommand(
+    "vscode-cheatsheet.closeCheatSheet",
+    () => {
+      if (currentPanel) {
+        currentPanel.dispose();
+      }
+    }
+  );
+
+  context.subscriptions.push(showCheatSheetCommand, closeCheatSheetCommand);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
+}
+
+export function deactivate() {
+  return;
+}
